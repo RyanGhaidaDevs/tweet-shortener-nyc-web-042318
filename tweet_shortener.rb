1d817349cf908f60f9ef53bc4203003b777753a2
Ryan Ghaida <ryaboug@gmail.com>
2018-04-11 18:27:26 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def dictionary_method()
+
+
+  dictionary = {"hello" => "hi","to, two, too" => "2","for, four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
+
+
+end
+
+def word_substituter(tweet)
+  i = 0 
+  string = ""
+  tweet_arr = []
+  tweet_arr = tweet.downcase.gsub(/[^a-z0-9\s]/i, '').split(" ")
+  arr2 = [] 
+  
+  
+  tweet_arr.collect do |word|
+    
+    dictionary_method.each do |key, replacements|
+      
+      if word == key 
+         
+         word = replacements 
+         
+      end 
+        
+        
+     end 
+      arr2 << word 
+  end 
+ arr2.join(" ") 
+ end
